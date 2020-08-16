b90ae786f395306fa5293905569dd4c0f2fedd60
Jaskaran Singh <yayjkay@gmail.com>
2020-08-16 15:39:12 +0530

    Completed

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, render_template
 import numpy as np
 import pickle
+from sklearn.preprocessing import LabelEncoder
 
 
 app = Flask(__name__, template_folder="templates")
 model = pickle.load(open('linear.pkl', 'rb'))
+# model = pickle.load(open('randomForest.pkl', 'rb'))
 
 
 @app.route("/")
@@ -14,8 +16,20 @@ def index():
 
 @app.route("/output", methods=["POST"])
 def output():
-    int_features = [x for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    # prediction = model.predict(final_features)
+    # select_inputs = ("terms", "verification_status", "Purpose", "State",
+    #                  "initial_list_status", "application_type")
+    # lb_make = LabelEncoder()
+    int_features = []
+    f = request.form
+    for key in f.keys():
+        for value in f.getlist(key):
+            int_features.append(value)
+    #         if(key not in select_inputs):
+    #             int_features.append(float(value))
+    #         else:
+    #             int_features.append(lb_make.fit_transform(value))
 
-    return render_template('output.html', prediction_text=int_features)
+    final_features = np.array(int_features, dtype="float64").reshape(1, -1)
+    prediction = model.predict(final_features)
+
+    return render_template('output.html', prediction_text=prediction[0][0])
--- predictor.py
+++ predictor.py
@@ -1,3 +1,4 @@
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
@@ -20,7 +21,9 @@ df1 = df[df.columns[df.isnull().any()]]
 df2 = df[df.columns[~df.isnull().any()]]
 
 df11 = df1.drop(['verification_status_joint'], axis=1)
-df12 = df1[['verification_status_joint']]
+# df12 = df1[['verification_status_joint']]
+
+n = df11.columns
 
 # Filling missing values to numerical variables
 mp_mean = SimpleImputer(missing_values=np.nan, strategy='mean')
@@ -28,43 +31,47 @@ mp_median = SimpleImputer(missing_values=np.nan, strategy='median')
 df11 = mp_median.fit_transform(df11)
 
 # filling missing values for categorical data
-df12 = df12.apply(lambda x: x.fillna(x.value_counts().index[0]))
+# df12 = df12.apply(lambda x: x.fillna(x.value_counts().index[0]))
 
 df13 = pd.DataFrame(df11)
 
-df = pd.concat([df2, df12, df13], axis=1)
+df13.columns = n
+
+df = pd.concat([df2, df13], axis=1)
 
 lb_make = LabelEncoder()
 
-df['terms'] = lb_make.fit_transform(df['terms'])
-df['verification_status'] = lb_make.fit_transform(df['verification_status'])
-df['purpose'] = lb_make.fit_transform(df['purpose'])
-df['State'] = lb_make.fit_transform(df['State'])
-df['initial_list_status'] = lb_make.fit_transform(df['initial_list_status'])
-df['application_type'] = lb_make.fit_transform(df['application_type'])
-df['last_week_pay'] = lb_make.fit_transform(df['last_week_pay'])
-df['verification_status_joint'] = lb_make.fit_transform(
-    df['verification_status_joint'])
+category_col = ['terms', 'verification_status', 'purpose', 'State', 'initial_list_status',
+                'application_type']
+
+for col in category_col:
+    df[col] = lb_make.fit_transform(df[col])
 
 y = df[['total revol_bal']]
-x = df.iloc[:, [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13,
-                14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]]
+x = df.iloc[:, [0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13,
+                15, 16, 17, 18, 19, 20, 21, 22, 23, 24]]
+
+a = x.columns
 
 scaler = StandardScaler()
 x = scaler.fit_transform(x)
-# a = x.columns
+
 x = pd.DataFrame(x)
-# x.columns = a
+x.columns = a
 
 
-kf = RepeatedKFold(n_splits=5, n_repeats=10, random_state=None)
+# kf = RepeatedKFold(n_splits=5, n_repeats=10, random_state=None)
 
-for train_index, test_index in kf.split(x):
-    x_train, x_test = x.iloc[train_index], x.iloc[test_index]
-    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+# for train_index, test_index in kf.split(x):
+#     x_train, x_test = x.iloc[train_index], x.iloc[test_index]
+#     y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
 
 ln = LinearRegression()
-lnfit = ln.fit(x_train, y_train)
+lnfit = ln.fit(x, y)
+
+model = RandomForestRegressor()
+model.fit(x, y)
 
 pickle.dump(lnfit, open('linear.pkl', 'wb'))
+pickle.dump(model, open('randomForest.pkl', 'wb'))
